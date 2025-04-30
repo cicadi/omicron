f7d4aca28c4c0143280726ae0a58f2f441c44862
cicadi <cicadi.dev@gmail.com>
2025-04-30 23:24:05 +0330

    Paths now remove trailing slash, and request handlers now have access to client socket address info.

--- crates/omicron/src/main.rs
+++ crates/omicron/src/main.rs
@@ -1,6 +1,9 @@
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 
+use axum::{ServiceExt, extract::Request};
 use tokio::net::TcpListener;
+use tower_http::normalize_path::NormalizePathLayer;
+use tower_layer::Layer;
 
 use omicron::{App, app::Config, error::Error};
 
@@ -9,10 +12,15 @@ async fn main() -> Result<(), Error> {
     dotenvy::dotenv()?;
 
     let app = Arc::new(App::new(Config::from_env()?)?);
-    let router = omicron::build_router(app.clone());
+    let router =
+        NormalizePathLayer::trim_trailing_slash().layer(omicron::build_router(app.clone()));
 
     let listener = TcpListener::bind(&app.config.server_url).await?;
     println!("> Server started. Listening on: {}", app.config.server_url);
 
-    Ok(axum::serve(listener, router).await?)
+    Ok(axum::serve(
+        listener,
+        ServiceExt::<Request>::into_make_service_with_connect_info::<SocketAddr>(router),
+    )
+    .await?)
 }
